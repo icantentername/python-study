534027aaa782b78ef1fc9e1a6b2e0c35f4db2031
icantentername <85990500+icantentername@users.noreply.github.com>
2021-07-08 17:41:55 +0800

    Update joseph_Ring.py

--- joseph_Ring.py
+++ joseph_Ring.py
@@ -1,11 +1,17 @@
-def joseph_Ring(num_people, skip_num):
-    if num_people == 1:
-        return(0)   
-    else:
-        return((joseph_Ring(num_people - 1, skip_num) + skip_num) % num_people)
+def josephus_ring(n,step):
 
-num_people = int(input('请输入总人数： '))
-skip_num = int(input('请输入要跳过的数字： '))
-result = joseph_Ring(num_people, skip_num)
+    link = list(range(1,n+1))       #给成员编号，从1开始
+    result = [0 for i in range(n)]  #初始化淘汰结果列表
+    rank = 0                        #初始化成员编号的索引值
 
-print('最后留下的人编号是: ', result)
+    for i in range(n):
+        rank = (rank + step) % len(link)
+        rank -= 1
+        result[i] = link[rank]
+        del link[rank]
+        if rank == -1:
+            rank = 0
+    
+    return result
+
+print(josephus_ring(5,3))
